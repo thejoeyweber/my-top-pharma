087df7e80f94e8b66fbf73d328c06ab3a6dca021
jaw_labs <joey@joeyweber.me>
2025-04-04 12:54:23 -0500

    Remove tracked files that should be ignored

--- .vercel/output/_functions/chunks/assetUtils_P7u5uQke.mjs
+++
@@ -1,9 +0,0 @@
-function getProductImageUrl(productId) {
-  return null;
-}
-function getWebsiteScreenshotUrl(websiteId) {
-  const baseUrl = "/assets";
-  return `${baseUrl}/websites/${websiteId}/screenshot.jpg`;
-}
-
-export { getWebsiteScreenshotUrl as a, getProductImageUrl as g };
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BYrIs_oY.mjs';
-export { renderers } from '../renderers.mjs';
